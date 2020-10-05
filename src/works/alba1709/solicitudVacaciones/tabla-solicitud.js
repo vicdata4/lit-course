2254147987d2d7d5c6588aeeb5aa05bc90c4be93
Alba Moreno <albamq1709@gmail.com>
2020-10-05 18:11:42 +0200

    media query en proceso

--- src/works/alba1709/solicitudVacaciones/tabla-solicitud.js
+++ src/works/alba1709/solicitudVacaciones/tabla-solicitud.js
@@ -11,13 +11,20 @@ export class TablaSolicitud extends LitElement {
               margin-top: 20px;
             }
             td, th {
-              border: 1px solid black;
+              border-right: solid 3px black;
+              border-left: solid 3px black;
             }
             #tablaSoli {
               border-collapse: collapse;
+              width: 100%;
+              height: 100%;
+              font-size: 0.8rem;
+              overflow: scroll;
+              overflow-y: auto;
+              empty-cells: hide;
             }
             #tablaSoli tr:nth-child(odd) {
-              background-color: #ebebeb;
+              background-color: #eeeeee;
             }
             #papelera {
                 width: 30px;
@@ -33,6 +40,17 @@ export class TablaSolicitud extends LitElement {
             }
             td, th {
               font-family: "Comic Sans MS", cursive, sans-serif;
+            }
+            th {
+              background-color: #cccccc;
+            }
+            @media (min-width: 768px) {
+              #tablaSoli {
+                width: 70%;
+              }
+               
+              
+
             }
           `
     ];
@@ -63,13 +81,50 @@ export class TablaSolicitud extends LitElement {
     }) 
   }*/
 
+  /*orderDate(col, order) {
+    const ths = this;
+    debugger;
+    this.miTabla.sort(function(o1, o2) {
+      debugger;
+    this.miTabla.sort((o1, o2) => {
+      if (
+        ths.getDate(o1[col], true).getTime() >
+        ths.getDate(o2[col], true).getTime()
+      ) {
+        debugger;
+        return 1;
+      } else if (
+        ths.getDate(o1[col], true).getTime() <
+        ths.getDate(o2[col], true).getTime()) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+  }*/
+
   render() {
     return html`
           <table id="tablaSoli">
               <tr>
-                <th>Fecha de solicitud</th>
-                <th>Fecha inicio</th>
-                <th>Fecha Fin</th>
+                <th>Fecha de solicitud
+                  <span>
+                    <span @click="${() => this.orderDate('solicitud', 'asc')}">&#9652;</span>
+                    <span @click="${() => this.orderDate('solicitud', 'desc')}">&#9662;</span>
+                  </span>
+                </th>
+                <th>Fecha Inicio
+                  <span>
+                    <span @click="${() => this.orderDate('solicitud', 'asc')}">&#9652;</span>
+                    <span @click="${() => this.orderDate('solicitud', 'desc')}">&#9662;</span>
+                  </span>
+                </th>
+                <th>Fecha Fin
+                  <span>
+                    <span @click="${() => this.orderDate('solicitud', 'asc')}">&#9652;</span>
+                    <span @click="${() => this.orderDate('solicitud', 'desc')}">&#9662;</span>
+                  </span>
+                </th>
                 <th>Estado de la solicitud</th>
                 <th>Fecha de estado</th>
                 <th>Eliminar</th>
@@ -77,12 +132,12 @@ export class TablaSolicitud extends LitElement {
             ${this.miTabla.map((item, i) => {
     return html`
               <tr>
-                <td>${item.fHoy.split('-').reverse().join('-')}</td>
+                <td>${item.fHoy.split('-').reverse().join('-')} ${item.hActual}</td>
                 <td>${item.infoFI.split('-').reverse().join('-')}</td>
                 <td>${item.infoFF.split('-').reverse().join('-')}</td>
                 <td>Pendiente de aprobación</td>
                 <td>${item.fHoy.split('-').reverse().join('-')}</td>
-                <td> <button id="btnPapelera" @click="${() => this.deleteItem(i)}"><img id = "papelera" src="/assets/images/papelera.png"></img></button></td>
+                <td> <button id="btnPapelera" @click="${() => this.deleteItem(i)}"><img id = "papelera" src="/assets/alba1709/papelera.png"></img></button></td>
               </tr>`;
   })}
           </table>
