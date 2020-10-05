3832eb01cdfab457e222f81cf7aa41b3b47f8f26
unknown <claracge@gmail.com>
2020-10-05 10:03:54 +0200

    Primer componente - import

--- src/works/calaverosa/calaverosa-page.js
+++ src/works/calaverosa/calaverosa-page.js
@@ -2,6 +2,7 @@ import { LitElement, html } from 'lit-element';
 import { commonStyles } from '../../utils/custom-styles';
 import '../../components/common-header';
 import '../../components/work-header';
+import '..components/item-011.js';
 
 class CalaverosaPage extends LitElement {
   static get styles() {
