0e67600afbc5531bc6e32f875bc0c32c1e85c937
Andres <ab.cdr.clase@gmail.com>
2020-10-06 16:26:16 +0200

    Puntos cumplidos pendiente de ampliacion

--- src/works/andresclase1234/andresclase1234-page.js
+++ src/works/andresclase1234/andresclase1234-page.js
@@ -3,6 +3,7 @@ import { commonStyles } from '../../utils/custom-styles';
 import '../../components/common-header';
 import '../../components/work-header';
 import './empleado-vacaciones/solicitud-vacaciones';
+import './admin-vacaciones/admin-vacaciones';
 
 class Andresclase1234Page extends LitElement {
   static get styles() {
@@ -18,6 +19,7 @@ class Andresclase1234Page extends LitElement {
         <work-header>Andresclase1234</work-header>
       </section>
       <solicitud-vacaciones></solicitud-vacaciones>
+      <admin-vacaciones></admin-vacaciones>
     `;
   }
 }
