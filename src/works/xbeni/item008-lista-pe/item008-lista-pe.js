52f8fd85139123c9396be70cf4d02dad909a1e47
XBeni <benii_beni@hotmail.com>
2020-10-06 18:03:42 +0200

    CODIGO ELIMINADO

--- src/works/xbeni/item008-lista-pe/item008-lista-pe.js
+++ src/works/xbeni/item008-lista-pe/item008-lista-pe.js
@@ -116,20 +116,6 @@ class BeniListaPe extends LitElement {
 
     // SOLICITUD AJAX QUE RPEGUNTE POR LA DESCRIPCION
     getDatosDescripcionPe(id_peticion);
-    /*
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var resultados = JSON.parse(this.response);
-                //SOLO HACE FALTA CARGAR EL DATO DE LA DESCRIPCION, EL TITULO Y FECHA SE SOLICITAN EN LA 1 CONSULTA AJAX
-                this.shadowRoot.getElementById(CONSTANTS_ITEM008.id_resultados_descripcion_publicacion).textContent = "DESCRIPCION PRUEBAA";
-            }
-        }
-        var parametros_enviar = 'id_peticion=' + id_peticion;
-        xhttp.open("POST", "RUTA EXTRACCION DE DATOS", true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send(parametros_enviar);
-        */
   }
 }
 
