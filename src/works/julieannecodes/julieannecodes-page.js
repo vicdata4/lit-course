b2cdd54cf58595af105d33e654660a7e42015558
julieanneCodes <j.o.131502@gmail.com>
2020-10-12 21:47:23 +0200

    irrelevant changes

--- src/works/julieannecodes/julieannecodes-page.js
+++ src/works/julieannecodes/julieannecodes-page.js
@@ -3,7 +3,6 @@ import { commonStyles } from '../../utils/custom-styles';
 import '../../components/common-header';
 import '../../components/work-header';
 import './approval-table/approval-table';
-import './formularioVacaciones/vacation-table';
 
 class JulieannecodesPage extends LitElement {
   static get styles() {
@@ -18,7 +17,6 @@ class JulieannecodesPage extends LitElement {
       <section class="container">
         <work-header>julieanneCodes</work-header>
         <approval-table></approval-table>
-          <vacation-table></vacation-table>
       </section>
     `;
   }
