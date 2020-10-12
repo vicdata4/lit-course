8c730bb1352de397fed49b7b329ff5058061947c
julieanneCodes <j.o.131502@gmail.com>
2020-10-12 21:50:20 +0200

    New file to import list of employees. Id added.

--- src/works/julieannecodes/utils/employees.js
+++ src/works/julieannecodes/utils/employees.js
@@ -1,81 +1,92 @@
 export const employeeList = [
-  { nombre: 'Juan Cuesta',
-    fSolicitud: new Date('2020/09/16'),
-    fInicio: new Date('2020/12/15'),
-    fFin: new Date('2020/01/12'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/09/16')
+  {
+    id: 0,
+    name: 'Juan Cuesta',
+    applicationD: new Date('2020/09/16'),
+    startDate: new Date('2020/12/15'),
+    endingDate: new Date('2020/01/12'),
+    status: 'No Aprobado',
+    statusDate: new Date('2020/09/16')
   },
   {
-    nombre: 'Mariano Rajoy',
-    fSolicitud: new Date('2020/08/15'),
-    fInicio: new Date('2020/09/11'),
-    fFin: new Date('2020/09/22'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/08/17')
+    id: 1,
+    name: 'Mariano Rajoy',
+    applicationD: new Date('2020/08/15'),
+    startDate: new Date('2020/09/11'),
+    endingDate: new Date('2020/09/22'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/08/17')
   },
   {
-    nombre: 'Pedro Sánchez',
-    fSolicitud: new Date('2020/08/04'),
-    fInicio: new Date('2020/10/15'),
-    fFin: new Date('2020/12/12'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/08/04')
+    id: 2,
+    name: 'Pedro Sánchez',
+    applicationD: new Date('2020/08/04'),
+    startDate: new Date('2020/10/15'),
+    endingDate: new Date('2020/12/12'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/08/04')
   },
   {
-    nombre: 'Juliana Ordóñez',
-    fSolicitud: new Date('2020/08/10'),
-    fInicio: new Date('2020/10/20'),
-    fFin: new Date('2020/12/12'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/08/10')
+    id: 3,
+    name: 'Juliana Ordóñez',
+    applicationD: new Date('2020/08/10'),
+    startDate: new Date('2020/10/20'),
+    endingDate: new Date('2020/12/12'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/08/10')
   },
   {
-    nombre: 'Laura Peña',
-    fSolicitud: new Date('2020/09/04'),
-    fInicio: new Date('2020/11/15'),
-    fFin: new Date('2020/11/30'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/09/04')
+    id: 4,
+    name: 'Laura Peña',
+    applicationD: new Date('2020/09/04'),
+    startDate: new Date('2020/11/15'),
+    endingDate: new Date('2020/11/30'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/09/04')
   },
   {
-    nombre: 'Jaime Aguilera',
-    fSolicitud: new Date('2020/09/10'),
-    fInicio: new Date('2020/10/20'),
-    fFin: new Date('2020/11/19'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/09/10')
+    id: 5,
+    name: 'Jaime Aguilera',
+    applicationD: new Date('2020/09/10'),
+    startDate: new Date('2020/10/20'),
+    endingDate: new Date('2020/11/19'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/09/10')
   },
   {
-    nombre: 'Marta Sánchez',
-    fSolicitud: new Date('2020/08/04'),
-    fInicio: new Date('2020/10/15'),
-    fFin: new Date('2020/12/12'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/08/04')
+    id: 6,
+    name: 'Marta Sánchez',
+    applicationD: new Date('2020/08/04'),
+    startDate: new Date('2020/10/15'),
+    endingDate: new Date('2020/12/12'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/08/04')
   },
   {
-    nombre: 'Elizabeth Benet',
-    fSolicitud: new Date('2020/08/08'),
-    fInicio: new Date('2020/10/02'),
-    fFin: new Date('2020/10/15'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/08/08')
+    id: 7,
+    name: 'Elizabeth Benet',
+    applicationD: new Date('2020/08/08'),
+    startDate: new Date('2020/10/02'),
+    endingDate: new Date('2020/10/15'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/08/08')
   },
   {
-    nombre: 'Pablo Iglesias',
-    fSolicitud: new Date('2020/08/17'),
-    fInicio: new Date('2021/01/15'),
-    fFin: new Date('2021/02/14'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/08/17')
+    id: 8,
+    name: 'Pablo Iglesias',
+    applicationD: new Date('2020/08/17'),
+    startDate: new Date('2021/01/15'),
+    endingDate: new Date('2021/02/14'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/08/17')
   },
   {
-    nombre: 'Adrian Carrazo',
-    fSolicitud: new Date('2020/10/05'),
-    fInicio: new Date('2020/12/15'),
-    fFin: new Date('2021/01/05'),
-    estado: 'Pendiente de aprobación',
-    fechaEstado: new Date('2020/10/05')
+    id: 9,
+    name: 'Adrián Carrazo',
+    applicationD: new Date('2020/10/05'),
+    startDate: new Date('2020/12/15'),
+    endingDate: new Date('2021/01/05'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/10/05')
   }
 ];
