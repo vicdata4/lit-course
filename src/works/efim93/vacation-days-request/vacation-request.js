ef36aec4703a2972cc93ac4ad45165bc7ace0cdc
Efim <ciubucefim@yahoo.com>
2020-10-12 22:06:35 +0200

    when adding request - structure pagination list

--- src/works/efim93/vacation-days-request/vacation-request.js
+++ src/works/efim93/vacation-days-request/vacation-request.js
@@ -27,13 +27,57 @@ class VacationRequest extends LitElement {
     td {
       min-width: 200px; 
     }
+
+    .order {
+      padding: 0;
+      background-color: transparent;
+      border: none;
+      margin-bottom: 10px;
+      cursor: pointer;
+    }
+      
+    .stepper {
+      margin: 10px 0;
+    }
+
+    .stepper .step:hover {
+      background-color: #f1f1f1;
+    }
+
+    .step {
+      display: inline-block;
+      padding: 5px;
+      border: 1px solid #d8d7d7;
+      width: 20px;
+      height: auto;
+      text-align: center;
+      cursor: pointer;
+    }
+
+    .step.active {
+      background-color: #535353 !important;
+      color: white;
+    }
+
+    .step.left {
+      transform: rotate(180deg);
+    }
+
+    .stepper, .step {
+      user-select: none;
+    }
     `];
   }
 
   static get properties() {
     return {
-      listaDatos: { type: Array },
-      mensaje: { type: String }
+      listaDatos: { type: Array, attribute: true },
+      mensaje: { type: String },
+      nElements: { type: Number },
+      stepper: { type: Array, attribute: false },
+      index: { type: Number, attribute: false },
+      from: { type: Number, attribute: false },
+      to: { type: Number, attribute: false }
     };
   }
 
@@ -41,6 +85,11 @@ class VacationRequest extends LitElement {
     super();
     this.listaDatos = [];
     this.mensaje = '';
+    this.nElements = 10;
+    this.stepper = [];
+    this.from = 0;
+    this.to = this.nElements;
+    this.index = 0;
   }
 
   addArray() {
@@ -82,7 +131,7 @@ class VacationRequest extends LitElement {
 
   compruebaEstado(index) {
     for (const item in this.listaDatos) {
-      if ([item].includes(index)) {
+      if ([item].includes(index.toString())) {
         if (this.listaDatos[item].estado) {
           return true;
         }
@@ -125,6 +174,58 @@ class VacationRequest extends LitElement {
     }
   }
 
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName.includes('listaDatos')) {
+        const nPages = Math.ceil(this.listaDatos.length / this.nElements);
+        this.stepper = new Array(nPages).fill({});
+        this.to = this.nElements;
+        this.setActiveStep(this.index);
+      }
+    });
+  }
+
+  setActiveStep(index) {
+    this.shadowRoot.querySelectorAll('.step').forEach(row => {
+      if (row.id === `_${index}`) {
+        row.classList.add('active');
+      } else {
+        row.classList.remove('active');
+      }
+    });
+  }
+
+  showPage(index) {
+    this.index = index;
+    this.from = this.nElements * index;
+    this.to = this.from + this.nElements;
+    this.setActiveStep(index);
+  }
+
+  next() {
+    if (this.index < this.stepper.length - 1) {
+      this.showPage(this.index + 1);
+    }
+  }
+
+  prev() {
+    if (this.index > 0) {
+      this.showPage(this.index - 1);
+    }
+  }
+
+  renderStepper() {
+    return html`
+      <div class="stepper">
+        <div class="step left" @click="${this.prev}">&#x25B7;</div>
+        ${this.stepper.map((x, i) => html`
+          <div id="${`_${i}`}" class="step" @click="${() => this.showPage(i)}">${i + 1}</div>
+        `)}
+        <div class="step" @click="${this.next}">&#x25B7;</div>
+      </div>
+    `;
+  }
+
   render() {
     return html`
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -148,19 +249,19 @@ class VacationRequest extends LitElement {
             <th>Eliminar</th>
           </tr>
           </thead>
-           <tbody>     
-        ${this.listaDatos.map((item, i) => html`
-          <tr>
-          <td>${item.fsolicitud}</td>
-          <td>${item.inicio}</td>
-          <td>${item.final}</td>
-          <td>${item.estado ? 'Aprobado' : 'Pendiente de Aprobación'}</td>
-          <td>${item.festado}</td>
-          <td><button @click="${() => this.deleteArray(i)}">borrar</button></td>
-          </tr>
-          `)}
+           <tbody>   
+        ${this.listaDatos.slice(this.from, this.to).map((item, i) => html`  
+            <tr>
+            <td>${item.fsolicitud}</td>
+            <td>${item.inicio}</td>
+            <td>${item.final}</td>
+            <td>${item.estado ? 'Aprobado' : 'Pendiente de Aprobación'}</td>
+            <td>${item.festado}</td>
+            <td><button @click="${() => this.deleteArray(i)}">borrar</button></td>
+            </tr> `)}
           </tbody>       
         </table>
+          <div id="paginator">${this.renderStepper()}</div>
 </section>`;
   }
 }
