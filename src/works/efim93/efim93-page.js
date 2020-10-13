6977e8677223291c65a3d6cb2b2cdd1378d4bfc1
Efim <ciubucefim@yahoo.com>
2020-10-13 19:43:38 +0200

    inicial structure

--- src/works/efim93/efim93-page.js
+++ src/works/efim93/efim93-page.js
@@ -2,6 +2,7 @@ import { LitElement, html } from 'lit-element';
 import { commonStyles } from '../../utils/custom-styles';
 import '../../components/common-header';
 import '../../components/work-header';
+import './holiday-approval/holiday-approval';
 
 class Efim93Page extends LitElement {
   static get styles() {
@@ -15,6 +16,7 @@ class Efim93Page extends LitElement {
       <common-header></common-header>
       <section class="container">
         <work-header>efim93</work-header>
+        <holiday-approval></holiday-approval>
       </section>
     `;
   }
