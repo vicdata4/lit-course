e24414aedee6539d5c612bd7959835c247e59331
XBeni <benii_beni@hotmail.com>
2020-10-13 17:06:16 +0200

    XBeni - Nombre constantes cambiado formato camelCase()

--- src/works/xbeni/archivos_comunes/ac_informe-candidatos-informacion-desactualizada/constantes.js
+++ src/works/xbeni/archivos_comunes/ac_informe-candidatos-informacion-desactualizada/constantes.js
@@ -1,13 +1,11 @@
-
 export const CONSTANTS_ITEM002 = {
-  titulo_formulario: 'Lista de candidatos con información pendiente a actualizar',
   table_id: 'table_cipa',
-  div_body_cipa: 'div_body_cipa',
-  div_body_abrir_cipa: 'div_body_abrir_cipa',
-  label_ordenar_nombre_id: 'label_ordenar_nombre_id',
-  label_ordenar_correo_id: 'label_ordenar_correo_id',
-  label_ordenar_perfil_id: 'label_ordenar_perfil_id',
-  label_ordenar_plantilla_id: 'label_ordenar_plantilla_id',
-  label_ordenar_fua_id: 'label_ordenar_fua_id',
-  label_ordenar_fv_id: 'label_ordenar_fv_id'
+  divBodyCipa: 'div_body_cipa',
+  divBodyAbrirCipa: 'div_body_abrir_cipa',
+  labelOrdenarNombreId: 'label_ordenar_nombre_id',
+  labelOrdenarCorreoId: 'label_ordenar_correo_id',
+  labelOrdenarPerfilId: 'label_ordenar_perfil_id',
+  labelOrdenarPlantillaId: 'label_ordenar_plantilla_id',
+  labelOrdenarFuaId: 'label_ordenar_fua_id',
+  labelOrdenarFvId: 'label_ordenar_fv_id'
 };
