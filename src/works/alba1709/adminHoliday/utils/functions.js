8b55b26b33dbdd520448dd1b421c9897a4720297
Alba Moreno <albamq1709@gmail.com>
2020-10-15 10:27:51 +0200

    Month changed to month+1

--- src/works/alba1709/adminHoliday/utils/functions.js
+++ src/works/alba1709/adminHoliday/utils/functions.js
@@ -2,7 +2,7 @@ export const getFormatterDate = (date_) => {
   const date = new Date(date_);
 
   let day = date.getDate();
-  let month = date.getMonth();
+  let month = date.getMonth() + 1;
   const year = date.getFullYear();
   if (day < 10) {
     day = '0' + day;
