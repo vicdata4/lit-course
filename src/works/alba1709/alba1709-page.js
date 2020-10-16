3ed97bbee95186d15b87b9c0d688a69a9fb2f427
Alba Moreno <albamq1709@gmail.com>
2020-10-16 11:41:14 +0200

    Alba's page changed

--- src/works/alba1709/alba1709-page.js
+++ src/works/alba1709/alba1709-page.js
@@ -2,7 +2,13 @@ import { LitElement, html } from 'lit-element';
 import { commonStyles } from '../../utils/custom-styles';
 import '../../components/common-header';
 import '../../components/work-header';
+import './solicitudVacaciones/solicitud-vacaciones-view';
 import './adminHoliday/admin-holiday-view';
+
+const components = {
+  solicitudVacaciones: () => html`<solicitud-vacaciones-view>h</solicitud-vacaciones-view>`,
+  adminHoliday: () => html`<admin-holiday-view></admin-holiday-view>`
+};
 
 class Alba1709Page extends LitElement {
   static get styles() {
@@ -11,12 +17,32 @@ class Alba1709Page extends LitElement {
     ];
   }
 
+  static get properties() {
+    return {
+      current: { type: String, attribute: false }
+    };
+  }
+
+  constructor() {
+    super();
+    this.current = 'solicitudVacaciones';
+  }
+
+  setComponent(component) {
+    this.current = component;
+  }
+
   render() {
     return html`
       <common-header></common-header>
       <section class="container">
         <work-header>alba1709</work-header>
-        <admin-holiday-view></admin-holiday-view>
+        <div class="common-list">
+          ${Object.keys(components).map(item => html`
+            <button class="common-btn" @click="${() => this.setComponent(item)}">${item}</button>
+          `)}
+        </div>
+        ${components[this.current]()}
       </section>
     `;
   }
