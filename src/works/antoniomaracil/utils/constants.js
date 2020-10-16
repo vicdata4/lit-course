5a0890615b682527f3b96a5e5f8181f31bb10126
Antonio <antonio.maracil@gmail.com>
2020-10-16 10:17:11 +0200

    Mock data

--- src/works/antoniomaracil/utils/constants.js
+++ src/works/antoniomaracil/utils/constants.js
@@ -6,16 +6,16 @@ export const empData = [
     startDate: new Date(2020, 8, 25),
     endDate: new Date(2020, 8, 28),
     status: 'Pendiente de aprobación',
-    statusDate: new Date(2020, 11, 20)
+    statusDate: new Date(2020, 7, 15)
   },
   {
     id: 2,
     name: 'Bruno Ferrero',
     applicationDate: new Date(2020, 7, 15),
-    startDate: new Date(2020, 11, 15),
-    endDate: new Date(2020, 11, 20),
+    startDate: new Date(2020, 11, 2),
+    endDate: new Date(2020, 11, 8),
     status: 'Pendiente de aprobación',
-    statusDate: new Date(2020, 11, 20)
+    statusDate: new Date(2020, 7, 15)
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ export const empData = [
     startDate: new Date(2021, 0, 15),
     endDate: new Date(2021, 0, 21),
     status: 'Pendiente de aprobación',
-    statusDate: new Date(2020, 11, 20)
+    statusDate: new Date(2020, 8, 14)
   },
   {
     id: 4,
@@ -38,11 +38,11 @@ export const empData = [
   {
     id: 5,
     name: 'Dario Espinosa',
-    applicationDate: new Date(2020, 7, 15),
-    startDate: new Date(2020, 7, 15),
-    endDate: new Date(2020, 7, 15),
+    applicationDate: new Date(2020, 7, 3),
+    startDate: new Date(2020, 7, 14),
+    endDate: new Date(2020, 7, 21),
     status: 'Pendiente de aprobación',
-    statusDate: new Date(2020, 11, 20)
+    statusDate: new Date(2020, 7, 3)
   },
   {
     id: 6,
@@ -56,7 +56,7 @@ export const empData = [
   {
     id: 7,
     name: 'Lucas Rivas',
-    applicationDate: new Date(2020, 7, 15),
+    applicationDate: new Date(2020, 6, 10),
     startDate: new Date(2020, 7, 15),
     endDate: new Date(2020, 7, 15),
     status: 'Pendiente de aprobación',
@@ -65,9 +65,9 @@ export const empData = [
   {
     id: 8,
     name: 'Oliva Macia',
-    applicationDate: new Date(2020, 7, 15),
-    startDate: new Date(2020, 7, 15),
-    endDate: new Date(2020, 7, 15),
+    applicationDate: new Date(2020, 9, 15),
+    startDate: new Date(2020, 9, 1),
+    endDate: new Date(2020, 9, 10),
     status: 'Pendiente de aprobación',
     statusDate: new Date(2020, 11, 20)
   },
