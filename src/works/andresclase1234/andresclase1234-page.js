e31528068b7aa4a77467dd89f5222e85facad4eb
Andres <ab.cdr.clase@gmail.com>
2020-10-20 16:20:06 +0200

    updated

--- src/works/andresclase1234/andresclase1234-page.js
+++ src/works/andresclase1234/andresclase1234-page.js
@@ -3,6 +3,15 @@ import { commonStyles } from '../../utils/custom-styles';
 import '../../components/common-header';
 import '../../components/work-header';
 import './empleado-vacaciones/solicitud-vacaciones';
+import './admin-vacaciones/admin-vacaciones';
+import './info-holidays/info-holidays';
+
+const dates = [
+  { id: 1, name: 'Andres', holidayFrom: new Date('2020-10-15'), holidayTo: new Date('2020-10-19') },
+  { id: 2, name: 'Juan', holidayFrom: new Date('2020-11-15'), holidayTo: new Date('2020-11-20') },
+  { id: 3, name: 'Eva', holidayFrom: new Date('2020-12-15'), holidayTo: new Date('2020-12-22') },
+  { id: 4, name: 'Luis', holidayFrom: new Date('2020-10-19'), holidayTo: new Date('2020-10-23') }
+];
 
 class Andresclase1234Page extends LitElement {
   static get styles() {
@@ -17,7 +26,7 @@ class Andresclase1234Page extends LitElement {
       <section class="container">
         <work-header>Andresclase1234</work-header>
       </section>
-      <solicitud-vacaciones></solicitud-vacaciones>
+      <info-holidays .list="${dates}" .nElements="${2}"></info-holidays>
     `;
   }
 }
