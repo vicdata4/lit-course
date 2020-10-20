725f95726992e89851a7f4d8c8ab3b51a261bba1
Antonio <antonio.maracil@gmail.com>
2020-10-20 12:02:33 +0200

    Added new component - added mock files

--- src/works/antoniomaracil/antoniomaracil-page.js
+++ src/works/antoniomaracil/antoniomaracil-page.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { LitElement, html } from 'lit-element';
 import { commonStyles } from '../../utils/custom-styles';
-import { empData, vacationDays, empHistory } from './utils/constants';
+import { empData, vacationDays, empHistory, empDocument } from './utils/constants';
 import '../../components/common-header';
 import '../../components/work-header';
 import './components/vacation-form/components/vacation-form';
@@ -9,13 +9,15 @@ import './components/admin-vacation-form/components/admin-vacation-form';
 import './components/vacation-detail/components/vacation-detail';
 import './components/admin-vacation-detail/components/admin-vacation-detail';
 import './components/document-form/components/document-form';
+import './components/document-list/components/document-list';
 
 const components = {
   vacationForm: () => html`<vacation-form @update-array="${this.updateArray}" .nElements="${10}"></vacation-form>`,
   adminVacationForm: () => html`<admin-vacation-form .list="${empData}" .nElements="${10}" @update-array="${this.updateArray}"></admin-vacation-form>`,
   vacationDetail: () => html`<vacation-detail .list="${vacationDays}"></vacation-detail>`,
   adminVacationDetail: () => html`<admin-vacation-detail .list="${vacationDays}" .history="${empHistory}"></admin-vacation-detail>`,
-  documentForm: () => html`<document-form></document-form>`
+  documentForm: () => html`<document-form></document-form>`,
+  documentList: () => html`<document-list .list="${empDocument}"></document-list>`
 };
 
 class AntoniomaracilPage extends LitElement {
