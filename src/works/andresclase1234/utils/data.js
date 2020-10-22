3f921e31ddf29dfee253f7b929b78d65cb0eba56
Andres <ab.cdr.clase@gmail.com>
2020-10-22 13:58:22 +0200

    Updated with data file.

---
+++ src/works/andresclase1234/utils/data.js
@@ -0,0 +1,12 @@
+export const dates = [
+  { id: 1, holidayFrom: new Date('2020-10-15'), holidayTo: new Date('2020-10-19') },
+  { id: 2, holidayFrom: new Date('2020-11-15'), holidayTo: new Date('2020-11-20') },
+  { id: 3, holidayFrom: new Date('2020-12-15'), holidayTo: new Date('2020-12-22') },
+  { id: 4, holidayFrom: new Date('2020-10-19'), holidayTo: new Date('2020-10-23') },
+  { id: 5, holidayFrom: new Date('2020-10-20'), holidayTo: new Date('2020-10-30') },
+  { id: 6, holidayFrom: new Date('2020-11-01'), holidayTo: new Date('2020-11-14') },
+  { id: 7, holidayFrom: new Date('2020-11-02'), holidayTo: new Date('2020-11-05') },
+  { id: 8, holidayFrom: new Date('2020-11-11'), holidayTo: new Date('2020-11-24') },
+  { id: 9, holidayFrom: new Date('2020-11-25'), holidayTo: new Date('2020-11-30') },
+  { id: 10, holidayFrom: new Date('2020-11-30'), holidayTo: new Date('2020-12-08') }
+];
