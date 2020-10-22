c646fa091010a860f724049afdc9ef3bd9dddb5f
Efim <ciubucefim@yahoo.com>
2020-10-22 14:00:10 +0200

    delete duplicate styles

--- src/works/efim93/components/vacation-detail/vacation-detail-admin.js
+++ src/works/efim93/components/vacation-detail/vacation-detail-admin.js
@@ -7,56 +7,7 @@ import { commonStyles } from '../../utils/common-styles';
 class VacationDetailAdmin extends LitElement {
   static get styles() {
     return [
-      newStyles, commonStyles,
-      css`
-    .order {
-      padding: 0;
-      background-color: transparent;
-      border: none;
-      margin-bottom: 10px;
-      cursor: pointer;
-    }
-      
-    .stepper {
-      margin: 10px 0;
-    }
-
-    .stepper .step:hover {
-      background-color: #f1f1f1;
-    }
-
-    .step {
-      display: inline-block;
-      padding: 5px;
-      border: 1px solid #d8d7d7;
-      width: 20px;
-      height: auto;
-      text-align: center;
-      cursor: pointer;
-    }
-
-    .step.active {
-      background-color: #535353 !important;
-      color: white;
-    }
-
-    .step.left {
-      transform: rotate(180deg);
-    }
-
-    .stepper, .step {
-      user-select: none;
-    }
-
-    .svg_sort, .svg_trash{
-      width: 15px;
-      height: auto;
-      margin: 0;
-      cursor: pointer;
-      overflow: visible;
-      fill: #000;
-    }
-    `];
+      newStyles, commonStyles];
   }
 
   static get properties() {
