1a05a87413b485a3b79e3ce67a07445124838081
Antonio <antonio.maracil@gmail.com>
2020-10-26 10:08:59 +0100

    Replace table with divs

--- src/works/antoniomaracil/components/document-list/components/document-list.js
+++ src/works/antoniomaracil/components/document-list/components/document-list.js
@@ -5,11 +5,111 @@ import { formatDate } from './../../../utils/functions';
 export class DocumentList extends LitElement {
   static get styles() {
     return css`
-      input[type='file'] {
-        color: transparent;
+      .container {
+        width: 100%;
+        font-family: 'Open Sans', sans-serif, Arial;
+        letter-spacing: 0.4px;
       }
-      img {
+
+      .row > * {
+        text-align: left;
+        padding-right: 10px;
+
+        height: 30px;
+        line-height: 30px;
+      }
+
+      .row > *,
+      .info-container {
+        white-space: nowrap;
+        overflow: hidden;
+      }
+
+      .row.header {
+        margin-bottom: 20px;
+      }
+
+      .row.header > * {
+        letter-spacing: 1.2px;
+        color: #959595;
+      }
+
+      .row {
+        display: flex;
+        flex-flow: row nowrap;
+        border-bottom: 1px solid #ebebeb;
+        padding: 10px 0px;
+      }
+
+      .row:first-child {
+        border: none;
+      }
+      .row:nth-child(even) {
+        background-color: #eeeeee;
+      }
+
+      .title {
+        width: 70%;
+        color: #285659;
+      }
+
+      .date {
+        display: none;
         width: 20%;
+      }
+
+      .light-grey {
+        color: #747474;
+      }
+
+      .info {
+        width: 10%;
+      }
+
+      .info-container {
+        display: block;
+      }
+
+      .row.data {
+        margin-bottom: 40px;
+      }
+      .italic {
+        font-style: italic;
+      }
+
+      @media (min-width: 768px) {
+        .title {
+          width: 40%;
+        }
+
+        .date {
+          display: block;
+        }
+
+        .info-container {
+          display: none;
+        }
+
+        .row {
+          padding: 20px 0px;
+        }
+
+        .row.header {
+          margin-bottom: 0px;
+        }
+
+        .row.data {
+          margin-bottom: 0px;
+        }
+
+        .row.data:hover {
+          background-color: #f5f6f7;
+        }
+      }
+
+      img {
+        width: 20px;
+        cursor: pointer;
       }
       button {
         border: none;
@@ -43,6 +143,9 @@ export class DocumentList extends LitElement {
       td {
         height: 2rem;
       }
+      .document-table:last-child {
+        text-align: center;
+      }
       .margin-btn {
         margin-top: 2rem;
         margin-bottom: 1rem;
@@ -73,6 +176,30 @@ export class DocumentList extends LitElement {
     return html`
       <div class="container">
         <div class="doc-box">
+          <div class="container">
+            <div class="row header">
+              <div class="title">Nombre de archivo</div>
+              <div class="date">Fecha de subida</div>
+              <div class="info">Eliminar</div>
+            </div>
+            ${this.list.map(
+              (item, i) => html`
+                <div class="info-container">
+                  <span class="light-grey italic">${formatDate(item.uploadDate)}</span>
+                </div>
+                <div class="row data">
+                  <div class="title"><a href="${item.path}" download>${item.name}</a></div>
+                  <div class="date light-grey">${formatDate(item.uploadDate)}</div>
+                  <div class="info">
+                    <button @click="${() => this.removeDocument(i)}">
+                      <img src="assets/antoniomaracil/remove.png" alt="remove" />
+                    </button>
+                  </div>
+                </div>
+              `,
+            )}
+          </div>
+
           <fieldset>
             <legend>Documentación</legend>
             <div class="table-box">
