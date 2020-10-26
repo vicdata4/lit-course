bcd7d8fa948b368366dc3bdbe881ca25049a5166
Antonio <antonio.maracil@gmail.com>
2020-10-26 10:13:20 +0100

    Extracted css from the component file

--- src/works/antoniomaracil/components/document-list/components/document-list.js
+++ src/works/antoniomaracil/components/document-list/components/document-list.js
@@ -1,108 +1,11 @@
 /* eslint-disable no-console */
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html } from 'lit-element';
+import { documentListStyles } from '../../../utils/custom-styles';
 import { formatDate } from './../../../utils/functions';
 
 export class DocumentList extends LitElement {
   static get styles() {
-    return css`
-      .container {
-        width: 100%;
-        font-family: 'Open Sans', sans-serif, Arial;
-        letter-spacing: 0.4px;
-      }
-
-      .row > * {
-        text-align: left;
-        padding-right: 10px;
-
-        height: 30px;
-        line-height: 30px;
-      }
-
-      .row > *,
-      .info-container {
-        white-space: nowrap;
-        overflow: hidden;
-      }
-
-      .row.header {
-        margin-bottom: 20px;
-      }
-
-      .row.header > * {
-        letter-spacing: 1.2px;
-        color: #959595;
-      }
-
-      .row {
-        display: flex;
-        flex-flow: row nowrap;
-        border-bottom: 1px solid #ebebeb;
-        padding: 10px 0px;
-      }
-
-      .row:first-child {
-        border: none;
-      }
-      .title {
-        width: 70%;
-        color: #285659;
-      }
-
-      .date {
-        display: none;
-        width: 20%;
-      }
-
-      .light-grey {
-        color: #747474;
-      }
-
-      .info {
-        width: 20%;
-      }
-
-      .info-container {
-        display: block;
-      }
-
-      .row.data {
-        margin-bottom: 40px;
-      }
-      .italic {
-        font-style: italic;
-      }
-
-      @media (min-width: 768px) {
-        .title {
-          width: 40%;
-        }
-
-        .date {
-          display: block;
-        }
-
-        .info-container {
-          display: none;
-        }
-
-        .row {
-          padding: 20px 0px;
-        }
-
-        .row.header {
-          margin-bottom: 0px;
-        }
-
-        .row.data {
-          margin-bottom: 0px;
-        }
-
-        .row.data:hover {
-          background-color: #f5f6f7;
-        }
-      }
-    `;
+    return [documentListStyles];
   }
 
   static get properties() {
