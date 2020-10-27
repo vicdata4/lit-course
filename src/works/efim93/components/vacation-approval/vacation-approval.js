bf9a7aab270914c62205b61cd0f6f3afd8854bf2
efim93 <ciubucefim@yahoo.com>
2020-10-27 12:45:10 +0100

    refactor the component and fixed css responsive

--- src/works/efim93/components/vacation-approval/vacation-approval.js
+++ src/works/efim93/components/vacation-approval/vacation-approval.js
@@ -8,9 +8,7 @@ import { newStyles } from '../../comun_files/table-responsive-styles';
 
 class VacationApproval extends LitElement {
   static get styles() {
-    return [
-      newStyles, commonStyles
-    ];
+    return [newStyles, commonStyles];
   }
 
   static get properties() {
@@ -35,7 +33,11 @@ class VacationApproval extends LitElement {
       Array.prototype.fill(formatDate(arr.fecha_estado).default, 5);
     }
     this.listaDatos = dataRequest;
-    this.options = [{ value: '0', text: 'Pendiente de Aprobación' }, { value: '1', text: 'Aprobado' }, { value: '2', text: 'No Aprobado' }];
+    this.options = [
+      { value: '0', text: 'Pendiente de Aprobación' },
+      { value: '1', text: 'Aprobado' },
+      { value: '2', text: 'No Aprobado' }
+    ];
     this.nElements = 10;
     this.stepper = [];
     this.from = 0;
@@ -47,7 +49,14 @@ class VacationApproval extends LitElement {
     const today = new Date();
     if (item.estado !== event.target.value) {
       if (this.listaDatos[i].nombre_apellido.includes(item.nombre_apellido)) {
-        this.listaDatos[i] = { nombre_apellido: this.listaDatos[i].nombre_apellido, fecha_solicitud: this.listaDatos[i].fecha_solicitud, fecha_inicio: this.listaDatos[i].fecha_inicio, fecha_fin: this.listaDatos[i].fecha_fin, estado: event.target.value, fecha_estado: formatDate(today).default };
+        this.listaDatos[i] = {
+          nombre_apellido: this.listaDatos[i].nombre_apellido,
+          fecha_solicitud: this.listaDatos[i].fecha_solicitud,
+          fecha_inicio: this.listaDatos[i].fecha_inicio,
+          fecha_fin: this.listaDatos[i].fecha_fin,
+          estado: event.target.value,
+          fecha_estado: formatDate(today).default
+        };
         this.listaDatos = [...this.listaDatos];
       }
     }
@@ -63,7 +72,7 @@ class VacationApproval extends LitElement {
   }
 
   setActiveStep(index) {
-    this.shadowRoot.querySelectorAll('.step').forEach(row => {
+    this.shadowRoot.querySelectorAll('.step').forEach((row) => {
       if (row.id === `_${index}`) {
         row.classList.add('active');
       } else {
@@ -100,7 +109,7 @@ class VacationApproval extends LitElement {
       return 0;
     });
 
-    if (JSON.stringify(this.listaDatos) === JSON.stringify((orderedList))) {
+    if (JSON.stringify(this.listaDatos) === JSON.stringify(orderedList)) {
       orderedList.reverse();
     }
 
@@ -112,9 +121,9 @@ class VacationApproval extends LitElement {
     return html`
       <div class="stepper">
         <div class="step left" @click="${this.prev}">&#x25B7;</div>
-        ${this.stepper.map((x, i) => html`
-          <div id="${`_${i}`}" class="step" @click="${() => this.showPage(i)}">${i + 1}</div>
-        `)}
+        ${this.stepper.map(
+    (x, i) => html` <div id="${`_${i}`}" class="step" @click="${() => this.showPage(i)}">${i + 1}</div> `
+  )}
         <div class="step" @click="${this.next}">&#x25B7;</div>
       </div>
     `;
@@ -122,48 +131,65 @@ class VacationApproval extends LitElement {
 
   render() {
     return html`
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <section>
-            <table id="tabla" class="table table-striped">
-            <caption>Solicitud de vacaciones de los Empleados</caption>
+            <table id="tabla">
                 <thead>
                     <tr>  
                         <th>
                             <label for="nombreApellido">Nombre Apellido</label>
-                            <button @click="${() => this.orderList('nombre_apellido')}" class="order" >${svgArrowsSort}</button>
+                            <button @click="${() =>
+    this.orderList('nombre_apellido')}" class="order" >${svgArrowsSort}</button>
                         </th>
                         <th>
                             <label for="fechadeSolicitud">Fecha de solicitud</label>
-                            <button @click="${() => this.orderList('fecha_solicitud')}" class="order" >${svgArrowsSort}</button>
+                            <button @click="${() =>
+    this.orderList('fecha_solicitud')}" class="order" >${svgArrowsSort}</button>
                         </th>
                         <th>
                             <label for="fechadeInicio">Fecha de inicio</label>
-                            <button @click="${() => this.orderList('fecha_inicio')}" class="order" >${svgArrowsSort}</button>
+                            <button @click="${() =>
+    this.orderList('fecha_inicio')}" class="order" >${svgArrowsSort}</button>
                         </th>
                         <th>
                             <label for="fechafinal">Fecha Final</label>
-                            <button @click="${() => this.orderList('fecha_fin')}" class="order" >${svgArrowsSort}</button></th>
+                            <button @click="${() =>
+    this.orderList('fecha_fin')}" class="order" >${svgArrowsSort}</button></th>
                         </th>
                         <th><label for="estado">Estado</label></th>
                         <th><label for="fechadeEstado">Fecha de Estado</label></th>
                     </tr>
                 </thead>
                 <tbody>   
-                    ${this.listaDatos.slice(this.from, this.to).map((item, i) => html`  
-                    <tr>
-                        <td><label for="${item.nombre_apellido}">${item.nombre_apellido}</label></td>
-                        <td><label for="${item.fecha_solicitud}">${item.fecha_solicitud}</label></td>
-                        <td><label for="${item.fecha_inicio}">${item.fecha_inicio}</label></td>
-                        <td><label for="${item.fecha_fin}">${item.fecha_fin}</td>
-                        <td>
-                        <select name="selectEstado" id="select" title="selectestado" @change="${(e) => this.onSelectChange(e, item, i)}" >
-                            ${this.options.map(option => html`
-                            <option selected="${ifDefined(option.value === item.estado ? 'true' : undefined)}" value="${option.value}" class="i${option.value}">${option.text}</option>
-                            `)} 
-                        </select>
-                        </td>
-                        <td><label for="${item.fecha_estado}">${item.fecha_estado}</label></td>
-                    </tr> `)}
+                    ${this.listaDatos.slice(this.from, this.to).map(
+    (item, i) => html`
+                        <tr class="${(i % 2) === 0 ? 'fila_par' : 'fila_impar'}">
+                          <td><label for="${item.nombre_apellido}">${item.nombre_apellido}</label></td>
+                          <td><label for="${item.fecha_solicitud}">${item.fecha_solicitud}</label></td>
+                          <td><label for="${item.fecha_inicio}">${item.fecha_inicio}</label></td>
+                          <td><label for="${item.fecha_fin}">${item.fecha_fin}</label></td>
+                          <td>
+                            <select
+                              name="selectEstado"
+                              id="select"
+                              title="selectestado"
+                              @change="${(e) => this.onSelectChange(e, item, i)}"
+                            >
+                              ${this.options.map(
+    (option) => html`
+                                  <option
+                                    selected="${ifDefined(option.value === item.estado ? 'true' : undefined)}"
+                                    value="${option.value}"
+                                    class="i${option.value}"
+                                  >
+                                    ${option.text}
+                                  </option>
+                                `
+  )}
+                            </select>
+                          </td>
+                          <td><label for="${item.fecha_estado}">${item.fecha_estado}</label></td>
+                        </tr>
+                      `
+  )}
                 </tbody>       
         </table>
         ${this.renderStepper()}
