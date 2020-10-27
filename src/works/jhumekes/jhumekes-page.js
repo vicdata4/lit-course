526db1f7e8d20215ab2a785e31c6f80055812f42
Jhumekes <jhumekes77@gmail.com>
2020-10-27 16:24:06 +0100

    Home page Jhumekes

--- src/works/jhumekes/jhumekes-page.js
+++ src/works/jhumekes/jhumekes-page.js
@@ -2,10 +2,32 @@ import { LitElement, html } from 'lit-element';
 import { commonStyles } from '../../utils/custom-styles';
 import '../../components/common-header';
 import '../../components/work-header';
+import '../jhumekes/visualizacion-peticiones';
+import '../jhumekes/descripcion-peticiones';
+
+const components = {
+  visualizacionPeticiones: () => html`<visualizacion-peticiones>Submit</visualizacion-peticiones>`,
+  // descripcionPeticiones: () => html`<descripcion-peticiones></descripcion-peticiones>`,
+};
 
 class JhumekesPage extends LitElement {
   static get styles() {
     return [commonStyles];
+  }
+
+  static get properties() {
+    return {
+      current: { type: String, attribute: false },
+    };
+  }
+
+  constructor() {
+    super();
+    this.current = 'visualizacionPeticiones';
+  }
+
+  setComponent(component) {
+    this.current = component;
   }
 
   render() {
@@ -13,7 +35,12 @@ class JhumekesPage extends LitElement {
       <common-header></common-header>
       <section class="container">
         <work-header>Jhumekes</work-header>
-        <visualizacion-peticiones></visualizacion-peticiones>
+        <div class="common-list">
+          ${Object.keys(components).map(
+            (item) => html` <button class="common-btn" @click="${() => this.setComponent(item)}">${item}</button> `,
+          )}
+        </div>
+        ${components[this.current]()}
       </section>
     `;
   }
