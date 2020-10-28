9843940f863a7b49025ac865a66dd72c55dac147
efim93 <ciubucefim@yahoo.com>
2020-10-28 15:12:22 +0100

    refactor component and css change

--- src/works/efim93/comun_files/table-responsive-styles.js
+++ src/works/efim93/comun_files/table-responsive-styles.js
@@ -1,99 +1,104 @@
 import { css } from 'lit-element';
 
 export const newStyles = css`
-  tabla {
+  #tabla {
     width: 100%;
     border-collapse: collapse;
-  }
-
-  tr:nth-of-type(odd) {
-    background: #eee;
-  }
-
-  th {
-    background: #333;
-    color: white;
-    font-weight: bold;
   }
 
   tr {
     text-align: center;
   }
 
+  .fila_par {
+    background-color: #bbb;
+  }
+
+  .fila_imppar {
+    background-color: lightgreen;
+  }
+
   td,
   th {
     padding: 6px;
     border: 1px solid #ccc;
-    text-align: left;
-  }
-
-  #select {
-    border: 1px solid #ccc;
-    width: 120px;
-    border-radius: 3px;
-    overflow: hidden;
   }
 
   @media only screen and (max-width: 760px), (min-device-width: 768px) and (max-device-width: 1024px) {
     label #select {
       font-size: 1.2em;
     }
+  }
 
-    table,
-    thead,
-    tbody,
-    th,
-    td,
-    tr {
-      display: block;
-    }
+  #select {
+    border: 1px solid #ccc;
+    width: 50%;
+    border-radius: 3px;
+    white-space: nowrap;
+    overflow: hidden !important;
+    text-overflow: ellipsis;
+  }
 
-    thead tr {
-      position: absolute;
-      top: -9999px;
-      left: -9999px;
-    }
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
 
-    tr {
-      border: 1px solid #ccc;
-    }
+  thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
 
-    td {
-      border: none;
-      border-bottom: 1px solid #eee;
-      position: relative;
-      padding-left: 50%;
-    }
+  tr {
+    border: 1px solid #ccc;
+  }
 
-    .i0 .i1 .i2 {
-      width: 120px;
-    }
-    td:before {
-      position: relative;
-      top: 0px;
-      left: 30px;
-      width: 85%;
-      padding-right: 40px;
-      white-space: nowrap;
-    }
+  td {
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 50%;
+    white-space: nowrap;
+    overflow: hidden !important;
+    text-overflow: ellipsis;
+  }
 
-    td:nth-of-type(1):before {
-      content: 'Nombre Apellido:';
-    }
-    td:nth-of-type(2):before {
-      content: 'Fecha de Solicitud:';
-    }
-    td:nth-of-type(3):before {
-      content: 'Fecha de Inicio:';
-    }
-    td:nth-of-type(4):before {
-      content: 'Fecha Fin:';
-    }
-    td:nth-of-type(5):before {
-      content: 'Estado:';
-    }
-    td:nth-of-type(6):before {
-      content: 'Fecha de estado:';
-    }
+  .i0 .i1 .i2 {
+    width: 120px;
+  }
+
+  td:before {
+    position: absolute;
+    top: 20%;
+    left: 3%;
+    width: 50%;
+    text-aling: right;
+    white-space: nowrap;
+    overflow: hidden !important;
+    text-overflow: ellipsis;
+  }
+
+  td:nth-of-type(1):before {
+    content: 'Nombre Apellido:';
+  }
+  td:nth-of-type(2):before {
+    content: 'Fecha de Solicitud:';
+  }
+  td:nth-of-type(3):before {
+    content: 'Fecha de Inicio:';
+  }
+  td:nth-of-type(4):before {
+    content: 'Fecha Fin:';
+  }
+  td:nth-of-type(5):before {
+    content: 'Estado:';
+  }
+  td:nth-of-type(6):before {
+    content: 'Fecha de estado:';
   }
 `;
