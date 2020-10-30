97a66f497f5e8e2f529c052b93273c1635b626cf
iKeyvin <ikeyvin@gmail.com>
2020-10-30 09:51:11 +0100

    Css lint fixed

--- src/works/ikeyvin/utils/costum-css.js
+++ src/works/ikeyvin/utils/costum-css.js
@@ -172,36 +172,6 @@ export const modalPopup = css`
     display: block;
   }
 
-  .modal-content {
-    box-sizing: border-box;
-  }
-
-  .modal-title {
-    padding-right: 30%;
-    font-weight: bold;
-    float: left;
-  }
-
-  .modal-date {
-    font-weight: bold;
-    padding-right: 30px;
-    float: left;
-  }
-
-  .modal-items {
-    width: 80%;
-  }
-
-  p {
-    font-weight: bold;
-    width: 30%;
-  }
-
-  textarea {
-    width: 100%;
-    height: 200px;
-  }
-
   .clearBoth {
     clear: both;
   }
@@ -220,65 +190,70 @@ export const modalPopup = css`
     cursor: pointer;
   }
 
-  * {
-  box-sizing: border-box;
-}
-
-input[type=text], select, textarea {
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  resize: vertical;
-}
-
-label {
-  padding: 12px 12px 12px 0;
-  display: inline-block;
-}
-
-input[type=submit] {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  float: right;
-}
-
-input[type=submit]:hover {
-  background-color: #45a049;
-}
-
-.container {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
-
-.col-25 {
-  float: left;
-  width: 25%;
-  margin-top: 6px;
-}
-
-.col-75 {
-  float: left;
-  width: 75%;
-  margin-top: 6px;
-}
-
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-@media screen and (max-width: 600px) {
-  .col-25, .col-75, input[type=submit] {
-    width: 100%;
-    margin-top: 0;
+  .modal {
+    box-sizing: border-box;
   }
 
+  input[type='text'],
+  select,
+  textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+  }
+
+  label {
+    padding: 12px 12px 12px 0;
+    display: inline-block;
+  }
+
+  input[type='submit'] {
+    background-color: #4caf50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    float: right;
+  }
+
+  input[type='submit']:hover {
+    background-color: #45a049;
+  }
+
+  .container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+    margin: 20%;
+  }
+
+  .col-25 {
+    float: left;
+    width: 20%;
+    margin-top: 6px;
+  }
+
+  .col-75 {
+    float: left;
+    width: 70%;
+    margin-top: 6px;
+  }
+
+  .row:after {
+    content: '';
+    display: table;
+    clear: both;
+  }
+
+  @media screen and (max-width: 600px) {
+    .col-25,
+    .col-75,
+    input[type='submit'] {
+      width: 100%;
+      margin-top: 0;
+    }
+  }
 `;
