a65d1e943da0fde523a7ddc7528ae9237aca0f51
Alba Moreno <albamq1709@gmail.com>
2020-11-06 16:36:58 +0100

    feat: functions and constants added

---
+++ src/works/alba1709/solicitudVacaciones/utils/constants.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    fHoy: new Date('2020/11/03'),
+    infoFI: new Date('2020/11/12'),
+    infoFF: new Date('2020/11/20'),
+    status: 'No Aprobado',
+    statusDate: new Date('2020/11/03'),
+  },
+  {
+    fHoy: new Date('2020/09/17'),
+    infoFI: new Date('2020/10/01'),
+    infoFF: new Date('2020/10/12'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/09/19'),
+  },
+  {
+    fHoy: new Date('2020/06/20'),
+    infoFI: new Date('2020/07/06'),
+    infoFF: new Date('2020/07/27'),
+    status: 'Pendiente de aprobación',
+    statusDate: new Date('2020/06/22'),
+  },
+];
