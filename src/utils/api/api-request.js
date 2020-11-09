1054b68242b87a007dfe92b72c8cc9a8b20909af
vicdata <vicdata@outlook.com>
2020-11-09 02:59:01 +0100

    refactor(api): common fetch

--- src/utils/api/api-request.js
+++ src/utils/api/api-request.js
@@ -1,55 +1,27 @@
-import { url, errorHandler } from './utils';
+import { url, commonFetch } from './utils';
 
 export const getInfo = async () => {
-  return fetch(`${url}/get-items`, { method: 'GET' })
-    .then(errorHandler)
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      return { error };
-    });
+  return commonFetch(`${url}/get-items`, { method: 'GET' });
 };
 
 export const addItem = async (data) => {
-  return fetch(`${url}/add-item`, {
+  return commonFetch(`${url}/add-item`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
-  })
-    .then(errorHandler)
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      return { error };
-    });
+  });
 };
 
 export const deleteItem = async (data) => {
-  return fetch(`${url}/delete-item/${data.id}`, {
+  return commonFetch(`${url}/delete-item/${data.id}`, {
     method: 'DELETE',
-  })
-    .then(errorHandler)
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      return { error };
-    });
+  });
 };
 
 export const updateItem = async (data) => {
-  return fetch(`${url}/update-item`, {
+  return commonFetch(`${url}/update-item`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
-  })
-    .then(errorHandler)
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      return { error };
-    });
+  });
 };
