a579a665b4782b171a5eb728f8b14a07baa1f68d
Andres <ab.cdr.clase@gmail.com>
2020-11-13 11:47:34 +0100

    style: removed some css

--- src/works/andresclase1234/components/admin-docs/admin-docs.js
+++ src/works/andresclase1234/components/admin-docs/admin-docs.js
@@ -9,40 +9,19 @@ export class AdminDocs extends LitElement {
         font-style: normal;
         font-size: 24px;
       }
-      .container {
-        width: 100%;
-        font-family: Roboto, 'Open Sans';
-      }
+
       table {
-        border: 1px solid #ccd0d4;
-        border-collapse: collapse;
-        margin-left: 1%;
-        margin-right: 1%;
-        width: 98%;
+        border: 1px solid #e4e4e4;
+        padding: 10px;
+        margin-left: 5%;
+        margin-right: 5%;
+        width: 90%;
       }
-      tr:first-of-type {
-        border: 1px solid #ccd0d4;
-      }
-      tr:nth-child(even) {
-        background-color: #f6f6f6;
-      }
-      .tdHeader {
-        line-height: 19.6px;
-        font-size: 14px;
-        text-size-adjust: 100%;
-        padding: 8px;
-        padding-left: 2px;
+
+      th {
         text-align: left;
       }
-      td {
-        font-weight: 500;
-        font-size: 13px;
-        text-size-adjust: 100%;
-      }
-      body {
-        color: #444;
-        line-height: 1.4em;
-      }
+
       button {
         background-color: transparent;
         border: none;
@@ -77,9 +56,9 @@ export class AdminDocs extends LitElement {
       <div class="container">
         <table>
           <tr>
-            <td class="tdHeader">Nombre de documento</td>
-            <td class="tdHeader">Fecha de carga</td>
-            <td class="tdHeader">Eliminar</td>
+            <td>Nombre de documento</td>
+            <td>Fecha de carga</td>
+            <td>Eliminar</td>
           </tr>
           ${this.list.map(
             (item, i) => html`
