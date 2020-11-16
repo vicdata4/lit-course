748b91dcaa3ddf3fcf3f047e5f799597ad5a7b01
Andres <ab.cdr.clase@gmail.com>
2020-11-16 11:29:17 +0100

    style: added styles

--- src/works/andresclase1234/components/admin-docs/admin-docs.js
+++ src/works/andresclase1234/components/admin-docs/admin-docs.js
@@ -1,54 +1,9 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html } from 'lit-element';
+import { adminStyle } from '../../utils/new-styles';
 
 export class AdminDocs extends LitElement {
   static get styles() {
-    return css`
-      .material-icons {
-        font-family: 'Material Icons';
-        font-weight: normal;
-        font-style: normal;
-        font-size: 24px;
-      }
-      .container {
-        width: 100%;
-        font-family: Roboto, 'Open Sans';
-      }
-      table {
-        border: 1px solid #ccd0d4;
-        border-collapse: collapse;
-        margin-left: 1%;
-        margin-right: 1%;
-        width: 98%;
-      }
-      tr:first-of-type {
-        border: 1px solid #ccd0d4;
-      }
-      tr:nth-child(even) {
-        background-color: #f6f6f6;
-      }
-      .tdHeader {
-        line-height: 19.6px;
-        font-size: 14px;
-        text-size-adjust: 100%;
-        padding: 8px;
-        padding-left: 2px;
-        text-align: left;
-      }
-      td {
-        font-weight: 500;
-        font-size: 13px;
-        text-size-adjust: 100%;
-      }
-      body {
-        color: #444;
-        line-height: 1.4em;
-      }
-      button {
-        background-color: transparent;
-        border: none;
-        cursor: pointer;
-      }
-    `;
+    return [adminStyle];
   }
 
   static get properties() {
