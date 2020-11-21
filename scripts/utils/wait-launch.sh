9bc4f2b09f6320b27e0eaebc4f02cfd805ec58c3
vicdata <vicdata@outlook.com>
2020-11-21 17:44:34 +0100

    fix: wait-launch.sh final config

--- scripts/utils/wait-launch.sh
+++ scripts/utils/wait-launch.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "Waiting app to launch on 2900..."
-
 while ! nc -z localhost 2900; do   
+  echo "Waiting app to launch on 2900..."
   sleep 0.1 # wait for 1/10 of the second before check again
 done
-
 echo "App launched"
+
+
+while [ ! -f ./build/main.js ]
+do
+  sleep 0.1 # or less like 0.2
+done
+echo "main.js is available"
