8a65de5ad86ca06889011856928f49de661b124c
isa <migt2frtic@gmail.com>
2021-04-15 17:42:04 +0200

    feat: open close funtion

--- src/components/dropdown-component.js
+++ src/components/dropdown-component.js
@@ -3,11 +3,39 @@ import { LitElement, html, css } from 'lit-element';
 
 class DropdownComponent extends LitElement {
   static get styles() {
-    return [css``];
+    return [
+      css`
+        .content {
+          display: none;
+          background-color: red;
+        }
+
+        .content.show {
+          display: block;
+        }
+      `,
+    ];
+  }
+
+  openCloseDropdown() {
+    const content = this.shadowRoot.querySelector('.content');
+
+    if (content.classList.contains('show')) {
+      content.classList.remove('show');
+    } else {
+      content.classList.add('show');
+    }
   }
 
   render() {
-    return html` <button class="btn-submit">dropdown</button> `;
+    return html`
+      <button class="btn-submit" @click="${this.openCloseDropdown}">dropdown</button>
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel luctus libero. Fusce et erat porttitor, mattis
+        neque id, rhoncus augue. Quisque vel interdum diam. Quisque aliquet hendrerit nulla eu accumsan. Pellentesque
+        turpis elit, accumsan consectetur felis ac, pulvinar tristique risus
+      </div>
+    `;
   }
 }
 
