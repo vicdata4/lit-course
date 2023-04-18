fdf0d37830d590493513d8e0034706c6dfb42559
Víctor González <victor.gonzalez.haya.contractor@bbva.com>
2023-04-18 22:48:17 +0200

    update docs

--- docs/main.js
+++ docs/main.js
@@ -8761,17 +8761,13 @@ const routing = (outlet) => {
   const router = new Router(outlet);
 
   var routes = [
-      { path: '/lit-course/',
-        component: 'home-view',
-        children: [
-          { path: '/validation', component: 'form-validation-view' },
-          { path: '/data-binding', component: 'data-binding' },
-          { path: '/profiles', component: 'profiles-view' },
-          { path: '/searcher', component: 'searcher-view' },
-          { path: '/storybook', component: 'storybook-view' },
-          { path: '(.*)', component: 'not-found-view' },
-      ]
-    },
+    { path: '/', component: 'home-view' },
+    { path: '/validation', component: 'form-validation-view' },
+    { path: '/data-binding', component: 'data-binding' },
+    { path: '/profiles', component: 'profiles-view' },
+    { path: '/searcher', component: 'searcher-view' },
+    { path: '/storybook', component: 'storybook-view' },
+    { path: '(.*)', component: 'not-found-view' },
   ];
 
   list.forEach((user) => {
--- src/routing.js
+++ src/routing.js
@@ -14,17 +14,13 @@ export const routing = (outlet) => {
   const router = new Router(outlet);
 
   var routes = [
-      { path: '/lit-course/',
-        component: 'home-view',
-        children: [
-          { path: '/validation', component: 'form-validation-view' },
-          { path: '/data-binding', component: 'data-binding' },
-          { path: '/profiles', component: 'profiles-view' },
-          { path: '/searcher', component: 'searcher-view' },
-          { path: '/storybook', component: 'storybook-view' },
-          { path: '(.*)', component: 'not-found-view' },
-      ]
-    },
+    { path: '/', component: 'home-view' },
+    { path: '/validation', component: 'form-validation-view' },
+    { path: '/data-binding', component: 'data-binding' },
+    { path: '/profiles', component: 'profiles-view' },
+    { path: '/searcher', component: 'searcher-view' },
+    { path: '/storybook', component: 'storybook-view' },
+    { path: '(.*)', component: 'not-found-view' },
   ];
 
   users.list.forEach((user) => {
