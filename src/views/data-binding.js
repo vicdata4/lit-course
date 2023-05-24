208c5470c8fa5af865c0a4b1d79b48cfb65009ea
Víctor González <victor.gonzalez.haya.contractor@bbva.com>
2023-05-24 16:31:47 +0200

    movie card

--- src/views/data-binding.js
+++ src/views/data-binding.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 import { commonStyles } from '../utils/custom-styles';
 import { store } from '../store/store';
 import { styles } from '../utils/home-styles';
@@ -12,13 +12,79 @@ import { addNote, deleteNote } from '../store/actions/notes.actions.js';
 
 class DataBinding extends connect(store)(LitElement) {
   static get styles() {
-    return [styles, commonStyles];
+    return [styles, commonStyles, css`
+    
+      .card-container {
+        display: flex;
+        flex-flow: row;
+      }
+
+      .card {
+        background-color: #ededed;
+        height: 100%;
+        width: 200px;
+        cursor: pointer;
+        display: flex;
+        overflow: hidden;
+      }
+      
+      .card-box {
+        position: relative;
+        margin: 10px;
+      }
+      
+      img {
+        width: 100%;
+        transition: all .5s;
+      }
+
+      .overlay {
+        opacity: 0;
+        position: absolute;
+        width: 100%;
+        top: 0;
+        bottom: 0;
+        background-color: #00000078;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: all .5s;
+      }
+
+      .card:hover img {
+        transform: scale(1.2);
+      }
+      
+      .card:hover .overlay {
+        opacity: 1;
+      }
+    `];
   }
 
   static get properties() {
     return {
       messageList: { type: Array },
+      cardList: { type: Array },
     };
+  }
+
+  constructor() {
+    super();
+    this.cardList = [
+      {
+        title: 'The Matrix Reloaded',
+        photo: 'https://m.media-amazon.com/images/M/MV5BODE0MzZhZTgtYzkwYi00YmI5LThlZWYtOWRmNWE5ODk0NzMxXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg'
+      },
+      {
+        title: 'Making The Matrix',
+        photo: 'https://m.media-amazon.com/images/M/MV5BMWRhNGY3NGQtMDAxMS00YjY2LTgzOTUtZjljZmUyYWQwMGI2XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg'
+      },
+      {
+        title: 'The Matrix: Path of Neo',
+        photo: 'https://m.media-amazon.com/images/M/MV5BZGFiNGU4MjEtODM2ZC00OTg0LThkNmEtZTBlN2FkMmFjOWYzXkEyXkFqcGdeQXVyNTEwNDY2MjU@._V1_SX300.jpg'
+      }
+    ]
   }
 
   addMessage(e) {
@@ -40,9 +106,21 @@ class DataBinding extends connect(store)(LitElement) {
       <common-header></common-header>
       <navigation-wc></navigation-wc>
       <section class="container">
-        <h1>Data binding</h1>
-        <input-form @my-event="${this.addMessage}">Guardar</input-form>
-        <list-component .list="${this.messageList}" @delete-event="${this.deleteMessage}"></list-component>
+        <div class="card-container">
+
+          ${this.cardList.map(x => html`
+
+          <div class="card-box">
+            <div class="card">
+              <img src="${x.photo}">
+              <div class="overlay">
+                ${x.title}
+              </div>
+            </div>
+
+          </div>
+          `)}
+        </div>
       </section>
     `;
   }
